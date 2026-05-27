2f690c6917284006ed957bf1fdb8fc136b63a132
arun <arundevops007@gmail.com>
2026-05-27 12:03:43 +0530

    add folder

---
+++ terraform_variable_block_28042026/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.69.0"
+    }
+  }
+  # backend "azurerm" {
+  #   resource_group_name  = "hello"
+  #   storage_account_name = "hellostore1001"
+  #   container_name       = "mycontainertest1"
+  #   key                  = "terraform.tfstate"
+
+  # }
+}
+
+provider "azurerm" {
+  features {}
+}
